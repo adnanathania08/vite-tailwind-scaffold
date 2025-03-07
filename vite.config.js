febe0b83137d1a6f423bea8f6a64572841f6c5a5
Adnan Athania <adnanathania9@gmail.com>
2025-03-07 11:47:38 +0530

    first commit

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react"; // Import the React plugin
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+});
